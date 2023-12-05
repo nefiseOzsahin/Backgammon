d26b8ea729f1fe8a21b8ad3ba81b9519cdb12018
nefiseOzsahin <ozsahinnefise@gmail.com>
2023-12-05 11:59:16 +0300

    ana sayfa düzenlendi

--- Backgammon/Controllers/HomeController.cs
+++ Backgammon/Controllers/HomeController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Backgammon.Business;
+using Backgammon.Context;
+using Backgammon.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backgammon.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {          
-            return View();
+        private readonly BackgammonContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly UserService _userService;
+
+
+        public HomeController(BackgammonContext context, UserManager<AppUser> userManager, UserService userService)
+        {
+            _context = context;
+            _userManager = userManager;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+
+            List<Tournament> tournaments = _context.Tournaments
+        .OrderByDescending(x => x.StartDate)
+        .Include(t => t.Users) // Eagerly load the Users collection
+        .ToList();
+            var nonAdminUsers = await _userService.GetNonAdminUsersAsync();
+            ViewBag.userCount= nonAdminUsers.Count();
+           ViewBag.tournamentCount= tournaments.Count();
+           
+
+
+
+            return View(tournaments);
         }
     }
 }
